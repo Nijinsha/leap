4c65cdcaae135eccfeaefe2762bcb668a9acf655
Nijinsha <mail.nijinsha@gmail.com>
2018-08-07 18:21:56 +0530

    add basic blockchain properties and methods

--- leap/blockchain.py
+++ leap/blockchain.py
@@ -1,2 +1,14 @@
 class Blockchain(object):
-    pass
+    """Blueprint of the blockchain object.
+    """
+    def __init__(self):
+        """Constructor to create a blockchain object.
+        """
+        self.chain = []
+        self.unspent_transactions = []
+        self.current_block_hash = None
+
+    def add_block_to_chain(self, block):
+        """Method to add a block to the chain.
+        """
+        self.chain.append(block)
